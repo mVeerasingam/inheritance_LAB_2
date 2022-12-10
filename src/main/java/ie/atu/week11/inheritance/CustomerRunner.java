4771d5deabe765ba08ebf49b088326cfbeb12ff0
Mark <G00403931@atu.ie>
2022-12-10 23:56:40 +0000

    added CustomerRunner class and customerRunner branch

---
+++ src/main/java/ie/atu/week11/inheritance/CustomerRunner.java
@@ -0,0 +1,8 @@
+package ie.atu.week11.inheritance;
+
+public class CustomerRunner {
+    public static void main(String[] args) {
+        Customer part1 = new Customer("Mark\t", "H65\t", "087\t", "1010\t", false);
+        System.out.println(part1);
+    }
+}
