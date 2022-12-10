f027c05eabf83723904dd7c3da0e6eac0885d34b
Mark <G00403931@atu.ie>
2022-12-10 23:59:07 +0000

    added part2

--- src/main/java/ie/atu/week11/inheritance/CustomerRunner.java
+++ src/main/java/ie/atu/week11/inheritance/CustomerRunner.java
@@ -4,5 +4,13 @@ public class CustomerRunner {
     public static void main(String[] args) {
         Customer part1 = new Customer("Mark\t", "H65\t", "087\t", "1010\t", false);
         System.out.println(part1);
+
+        Customer part2 = new Customer();
+        part2.setName("Liam\t");
+        part2.setAddress("H97\t");
+        part2.setTelephoneNumber("085\t");
+        part2.setCustomerNumber("2020\t");
+        part2.setMailingList(true);
+        System.out.println(part2);
     }
 }
