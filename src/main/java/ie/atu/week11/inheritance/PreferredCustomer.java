f1e12162f73589be7c1a59e2e0c63a943721977a
Mark <G00403931@atu.ie>
2022-12-11 00:10:00 +0000

    added PreferredCustomer constructor,getter+setters,toString and finance if statements

--- src/main/java/ie/atu/week11/inheritance/PreferredCustomer.java
+++ src/main/java/ie/atu/week11/inheritance/PreferredCustomer.java
@@ -1,4 +1,52 @@
 package ie.atu.week11.inheritance;
 
-public class PreferredCustomer {
+public class PreferredCustomer extends Customer {
+    private int priceAmount;
+    private int discountLevel;
+
+    public PreferredCustomer(int priceAmount) {
+        this.priceAmount=priceAmount;
+    }
+
+    public PreferredCustomer(String name, String address, String telephoneNumber, String customerNumber, boolean mailingList, int priceAmount, int discountLevel) {
+        super(name, address, telephoneNumber, customerNumber, mailingList);
+        this.priceAmount = priceAmount;
+        this.discountLevel = discountLevel;
+    }
+
+    public int getPriceAmount() {
+        return priceAmount;
+    }
+
+    public void setPriceAmount(int priceAmount) {
+        this.priceAmount = priceAmount;
+    }
+
+    public int getDiscountLevel() {
+        return discountLevel;
+    }
+
+    public void setDiscountLevel(int discountLevel) {
+        this.discountLevel = discountLevel;
+    }
+
+    public void customerDiscount(){
+        if((priceAmount>=500)&&(priceAmount>1000)){
+            discountLevel=5;
+        }
+        else if((priceAmount>=1000)&&(priceAmount>1500)){
+            discountLevel=6;
+        }
+        else if((priceAmount>=1500)&&(priceAmount>2000)){
+            discountLevel=7;
+        }
+        else if(priceAmount<=2000){
+            discountLevel=10;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return toString()+"PRICE AMMOUNT: " + "\n" +priceAmount + "DISCOUNT: " + discountLevel + "%";
+    }
 }
